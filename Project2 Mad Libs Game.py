f431b80ef511def01f5ebd40355522c4a87cb9ee
Md Abdullah Ibna Masud <anbimasud1@gmail.com>
2025-03-15 03:59:55 -0700

    Add files via upload
    Mad Libs Game in python

---
+++ Project2 Mad Libs Game.py
@@ -0,0 +1,13 @@
+username = input("what is your name: ")
+fathername =  input("what is your father name: ")
+mothername = input("what is your mother name: ")
+study = input("which class do you read in: " )
+relationship = input("tale me about your relationship status ")
+
+print(
+    f"My Doggy name is {username}\n,"
+    f"My Doggys Father name is {fathername}\n"
+    f"My Doggys Mother name is {mothername}\n"
+    f"My Doogy study in class {study}\n"
+    f"Doggys relationship status is {relationship}"
+)
